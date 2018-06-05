d0fca7cb26f90e4bcbc160e4b21510fd3131167c
TJTilot <tilottj@gmail.com>
2018-06-05 06:24:28 -0500

    walk logic added

---
+++ Components/DartballBL/DartballBL/GameEngine/Event/Implementation/GameEventWalkService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Dartball.BusinessLayer.GameEngine.Event.Dto;
+using Dartball.BusinessLayer.GameEngine.Event.Interface;
+using Dartball.BusinessLayer.GameEngine.Event.Interface.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dartball.BusinessLayer.GameEngine.Event.Implementation
+{
+    public class GameEventWalkService : IGameEventWalkService
+    {
+        private IMapper Mapper;
+
+        public GameEventWalkService()
+        {
+            var mapConfig = new MapperConfiguration(c => c.CreateMap<IHalfInningActions, HalfInningActionsDto>());
+            Mapper = mapConfig.CreateMapper();
+        }
+
+
+
+        public IHalfInningActions FillWalkActions(IHalfInningActions actions)
+        {
+            HalfInningActionsDto dto = Mapper.Map<HalfInningActionsDto>(actions);
+
+            if (AreBasesLoaded(dto) == true) dto.TotalRuns++;
+            else
+            {
+                if (dto.IsRunnerOnFirst && dto.IsRunnerOnSecond)
+                {
+                    dto.IsRunnerOnFirst = false;
+                    dto.IsRunnerOnThird = true;
+                }
+                else if (dto.IsRunnerOnFirst)
+                {
+                    dto.IsRunnerOnFirst = false;
+                    dto.IsRunnerOnSecond = true;
+                }
+            }
+            dto.IsRunnerOnFirst = true;
+
+            return dto;
+        }
+
+
+        private bool AreBasesLoaded(HalfInningActionsDto dto)
+        {
+            return dto.IsRunnerOnFirst && dto.IsRunnerOnSecond && dto.IsRunnerOnThird;
+        }
+
+
+    }
+}
---
+++ Tests/DartballBLUnitTest/DartballBLUnitTest/GameLogic/Event/GameEventWalkUnitTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dartball.BusinessLayer.GameEngine.Event.Dto;
+using Dartball.BusinessLayer.GameEngine.Event.Implementation;
+using Dartball.BusinessLayer.GameEngine.Event.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DartballBLUnitTest.GameLogic.Event
+{
+    [TestClass]
+    public class GameEventWalkUnitTests : EventBase
+    {
+        private IGameEventWalkService Service;
+
+        public GameEventWalkUnitTests()
+        {
+            Service = new GameEventWalkService();
+        }
+
+
+        [TestMethod]
+        public void BasesEmptyWalkTest()
+        {
+            HalfInningActionsDto dto = new HalfInningActionsDto();
+
+            var actions = Service.FillWalkActions(dto);
+            Assert.IsTrue(actions.IsRunnerOnFirst);
+            Assert.IsFalse(actions.IsRunnerOnSecond);
+            Assert.IsFalse(actions.IsRunnerOnThird);
+            Assert.IsTrue(actions.TotalRuns == 0);
+        }
+
+        [TestMethod]
+        public void RunnerOnFirstWalkTest()
+        {
+            HalfInningActionsDto dto = new HalfInningActionsDto
+            {
+                IsRunnerOnFirst = true
+            };
+
+            var actions = Service.FillWalkActions(dto);
+            Assert.IsTrue(actions.IsRunnerOnFirst);
+            Assert.IsTrue(actions.IsRunnerOnSecond);
+            Assert.IsFalse(actions.IsRunnerOnThird);
+            Assert.IsTrue(actions.TotalRuns == 0);
+        }
+
+        [TestMethod]
+        public void RunnerOnSecondWalkTest()
+        {
+            HalfInningActionsDto dto = new HalfInningActionsDto
+            {
+                IsRunnerOnSecond = true
+            };
+
+            var actions = Service.FillWalkActions(dto);
+            Assert.IsTrue(actions.IsRunnerOnFirst);
+            Assert.IsTrue(actions.IsRunnerOnSecond);
+            Assert.IsFalse(actions.IsRunnerOnThird);
+            Assert.IsTrue(actions.TotalRuns == 0);
+        }
+
+        [TestMethod]
+        public void RunnerOnThirdWalkTest()
+        {
+            HalfInningActionsDto dto = new HalfInningActionsDto
+            {
+                IsRunnerOnThird = true
+            };
+
+            var actions = Service.FillWalkActions(dto);
+            Assert.IsTrue(actions.IsRunnerOnFirst);
+            Assert.IsFalse(actions.IsRunnerOnSecond);
+            Assert.IsTrue(actions.IsRunnerOnThird);
+            Assert.IsTrue(actions.TotalRuns == 0);
+        }
+
+        [TestMethod]
+        public void RunnerOnFirstAndSecondWalkTest()
+        {
+            HalfInningActionsDto dto = new HalfInningActionsDto
+            {
+                IsRunnerOnFirst = true,
+                IsRunnerOnSecond = true
+            };
+
+            var actions = Service.FillWalkActions(dto);
+            Assert.IsTrue(actions.IsRunnerOnFirst);
+            Assert.IsTrue(actions.IsRunnerOnSecond);
+            Assert.IsTrue(actions.IsRunnerOnThird);
+            Assert.IsTrue(actions.TotalRuns == 0);
+        }
+
+        [TestMethod]
+        public void RunnerOnSecondAndThirdWalkTest()
+        {
+            HalfInningActionsDto dto = new HalfInningActionsDto
+            {
+                IsRunnerOnSecond = true,
+                IsRunnerOnThird = true
+            };
+
+            var actions = Service.FillWalkActions(dto);
+            Assert.IsTrue(actions.IsRunnerOnFirst);
+            Assert.IsTrue(actions.IsRunnerOnSecond);
+            Assert.IsTrue(actions.IsRunnerOnThird);
+            Assert.IsTrue(actions.TotalRuns == 0);
+        }
+
+
+        [TestMethod]
+        public void RunnerOnFirstAndThirdWalkTest()
+        {
+            HalfInningActionsDto dto = new HalfInningActionsDto
+            {
+                IsRunnerOnFirst = true,
+                IsRunnerOnThird = true
+            };
+
+            var actions = Service.FillWalkActions(dto);
+            Assert.IsTrue(actions.IsRunnerOnFirst);
+            Assert.IsTrue(actions.IsRunnerOnSecond);
+            Assert.IsTrue(actions.IsRunnerOnThird);
+            Assert.IsTrue(actions.TotalRuns == 0);
+        }
+
+        [TestMethod]
+        public void TheBasesLoadedWalkTest()
+        {
+            HalfInningActionsDto dto = new HalfInningActionsDto
+            {
+                IsRunnerOnFirst = true,
+                IsRunnerOnSecond = true,
+                IsRunnerOnThird = true
+            };
+
+            var actions = Service.FillWalkActions(dto);
+            Assert.IsTrue(actions.IsRunnerOnFirst);
+            Assert.IsTrue(actions.IsRunnerOnSecond);
+            Assert.IsTrue(actions.IsRunnerOnThird);
+            Assert.IsTrue(actions.TotalRuns == 1);
+        }
+
+    }
+}

ec89a98254880fbf7044c0f2d4ef14367125c28a
TJTilot <tilottj@gmail.com>
2018-06-20 06:51:03 -0500

    db tweak pushed out

--- Components/Dartball.Data/Dartball.Data/Migrations/20180620115004_IndexesAdded.Designer.cs
+++ Components/Dartball.Data/Dartball.Data/Migrations/20180620115004_IndexesAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Dartball.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dartball.Data.Migrations
 {
     [DbContext(typeof(DartballContext))]
-    partial class DartballContextModelSnapshot : ModelSnapshot
+    [Migration("20180620115004_IndexesAdded")]
+    partial class IndexesAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Components/Dartball.Data/Dartball.Data/Migrations/20180620115004_IndexesAdded.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dartball.Data.Migrations
+{
+    public partial class IndexesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerTeams_PlayerTeamId",
+                table: "PlayerTeams",
+                column: "PlayerTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameTeams_GameTeamId",
+                table: "GameTeams",
+                column: "GameTeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PlayerTeams_PlayerTeamId",
+                table: "PlayerTeams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GameTeams_GameTeamId",
+                table: "GameTeams");
+        }
+    }
+}

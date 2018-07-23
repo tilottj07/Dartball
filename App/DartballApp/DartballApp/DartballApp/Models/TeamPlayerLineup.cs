eebc636dbc55ea70bd9738a3b3c228542a947dba
travistilot <tilottj@gmail.com>
2018-07-22 20:41:54 -0500

    set lineup in progress

---
+++ App/DartballApp/DartballApp/DartballApp/Models/TeamPlayerLineup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Dartball.BusinessLayer.Team.Interface.Models;
+
+namespace DartballApp.Models
+{
+    public class TeamPlayerLineup
+    {
+        public TeamPlayerLineup(){}
+
+        public TeamPlayerLineup(ITeamPlayerLineup teamPlayerLineup)
+        {
+            if (teamPlayerLineup != null) {
+                TeamId = teamPlayerLineup.TeamId;
+                PlayerId = teamPlayerLineup.PlayerId;
+                BattingOrder = teamPlayerLineup.BattingOrder;
+            }
+        }
+
+        public Guid TeamId { get; set; }
+        public Guid PlayerId { get; set; }
+        public int BattingOrder { get; set; }
+
+    }
+}
---
+++ App/DartballApp/DartballApp/DartballApp/ViewModels/Team/TeamPlayerLineupViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dartball.BusinessLayer.Player.Implementation;
+using Dartball.BusinessLayer.Player.Interface;
+using Dartball.BusinessLayer.Shared.Models;
+using Dartball.BusinessLayer.Team.Dto;
+using Dartball.BusinessLayer.Team.Implementation;
+using Dartball.BusinessLayer.Team.Interface;
+using Dartball.BusinessLayer.Team.Interface.Models;
+
+namespace DartballApp.ViewModels.Team
+{
+    public class TeamPlayerLineupViewModel
+    {
+        ITeamPlayerLineupService TeamPlayerLineup;
+
+        public TeamPlayerLineupViewModel(Guid teamId)
+        {
+            TeamPlayerLineup = new TeamPlayerLineupService();
+            TeamId = teamId;
+        }
+
+        public Guid TeamId { get; set; }
+        public List<Models.Player> Batters { get; set; }
+
+
+
+        public void FillBatters() {
+            Batters = new List<Models.Player>();
+            foreach(var item in TeamPlayerLineup.GetTeamSortedBattingOrderPlayers(TeamId)) {
+                Batters.Add(new Models.Player(item));
+            }
+        }
+
+       
+        public ChangeResult SaveLineup() {
+            List<ITeamPlayerLineup> teamPlayerLineups = new List<ITeamPlayerLineup>();
+
+            int battingOrder = 0;
+            foreach(var batter in Batters) {
+                teamPlayerLineups.Add(new TeamPlayerLineupDto()
+                {
+                    TeamId = TeamId,
+                    PlayerId = batter.PlayerId,
+                    BattingOrder = battingOrder
+                });
+
+                battingOrder++;
+            }
+
+            return TeamPlayerLineup.SetLineup(teamPlayerLineups);
+        }
+
+       
+
+    }
+}
